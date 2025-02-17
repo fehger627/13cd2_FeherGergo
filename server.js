0172881d2c18a795f3595434d37b98b97af66203
fehger627 <fehger627@hengersor.hu>
2025-02-17 14:14:28 +0100

    server beállitása
    mysql server kapcsolatának létrehozása.
    ellenörzö get megirása

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ const mysql = require("mysql");
 const app = express();
 const cors = require("cors");
 const bodyparser = require("body-parser");
+const { request } = require("http");
 app.use(cors());
 app.use(bodyparser.json());
 
+const db = mysql.createConnection(
+    {
+        user: "root",
+        host: "localhost",
+        port: "3307",
+        password: "xd",
+        database:"teliolimpia",
+    }
+
+)
+app.get("/", (req, res) => {
+    res.send("szerver működik!")
+})
+
+//a szerver futásának ellenörzése
 
 
 app.listen(3000, () => {
